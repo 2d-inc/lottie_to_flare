c09c7a4c5ba731f25625dd68567efffba7089710
Luigi Rosso <luigi.rosso@gmail.com>
2019-06-16 15:57:26 -0700

    Initial checkin.

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./tool/index.html",
+            filename: "./index.html"
+        })
+    ],
+    entry: "./tool/index.js",
+    output: {
+        filename: '[name].bundle.js',
+        path: __dirname
+    }
+};
