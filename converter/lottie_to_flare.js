5b5620ff496f1ba5a9db31d28dbf9d066c39f0ca
Luigi Rosso <luigi.rosso@gmail.com>
2019-06-16 18:24:41 -0700

    Adding basic converter (hardcoded Flare file is generated right now) and UI.

---
+++ converter/lottie_to_flare.js
@@ -0,0 +1,223 @@
+export default class LottieToFlare
+{
+    constructor(options)
+    {
+
+    }
+
+    convert(json)
+    {
+        return new Promise((resolve, reject) =>
+        {
+            resolve({
+                "artboards": {
+                    "type": "artboards",
+                    "id": 985,
+                    "name": "Artboards",
+                    "main": 0,
+                    "children": [
+                        {
+                            "type": "artboard",
+                            "id": 986,
+                            "name": "Artboard",
+                            "translation": [
+                                0,
+                                0
+                            ],
+                            "origin": [
+                                0,
+                                0
+                            ],
+                            "width": 1024,
+                            "height": 768,
+                            "color": [
+                                0.364705890417099,
+                                0.364705890417099,
+                                0.364705890417099,
+                                1
+                            ],
+                            "clipContents": true,
+                            "animations": [],
+                            "children": [
+                                {
+                                    "type": "shape",
+                                    "id": 996,
+                                    "name": "Rectangle",
+                                    "translation": [
+                                        479.5,
+                                        407.5
+                                    ],
+                                    "blendMode": "srcOver",
+                                    "drawOrder": 3,
+                                    "clips": [
+                                        1000
+                                    ],
+                                    "children": [
+                                        {
+                                            "type": "colorStroke",
+                                            "id": 997,
+                                            "name": "Color",
+                                            "opacity": 1,
+                                            "color": [
+                                                0.800000011920929,
+                                                0.800000011920929,
+                                                0.800000011920929,
+                                                1
+                                            ],
+                                            "width": 1,
+                                            "cap": "butt",
+                                            "join": "miter",
+                                            "trim": "off",
+                                            "trimStart": 0,
+                                            "trimEnd": 1,
+                                            "trimOffset": 0
+                                        },
+                                        {
+                                            "type": "colorFill",
+                                            "id": 998,
+                                            "name": "Color",
+                                            "opacity": 1,
+                                            "color": [
+                                                1,
+                                                0,
+                                                0,
+                                                1
+                                            ],
+                                            "fillRule": "nonzero"
+                                        },
+                                        {
+                                            "type": "rectangle",
+                                            "id": 999,
+                                            "name": "Rectangle Path",
+                                            "width": 257,
+                                            "height": 257,
+                                            "cornerRadius": 0
+                                        }
+                                    ]
+                                },
+                                {
+                                    "type": "solo",
+                                    "id": 1000,
+                                    "name": "Solo Group",
+                                    "translation": [
+                                        596.5,
+                                        295.5
+                                    ],
+                                    "activeChildIndex": 0,
+                                    "children": [
+                                        {
+                                            "type": "shape",
+                                            "id": 987,
+                                            "name": "Ellipse",
+                                            "translation": [
+                                                -160.5,
+                                                14.5
+                                            ],
+                                            "blendMode": "srcOver",
+                                            "drawOrder": 1,
+                                            "children": [
+                                                {
+                                                    "type": "colorStroke",
+                                                    "id": 988,
+                                                    "name": "Color",
+                                                    "opacity": 1,
+                                                    "color": [
+                                                        0.800000011920929,
+                                                        0.800000011920929,
+                                                        0.800000011920929,
+                                                        1
+                                                    ],
+                                                    "width": 1,
+                                                    "cap": "butt",
+                                                    "join": "miter",
+                                                    "trim": "off",
+                                                    "trimStart": 0,
+                                                    "trimEnd": 1,
+                                                    "trimOffset": 0
+                                                },
+                                                {
+                                                    "type": "colorFill",
+                                                    "id": 989,
+                                                    "name": "Color",
+                                                    "opacity": 1,
+                                                    "color": [
+                                                        0.4000000059604645,
+                                                        0.4000000059604645,
+                                                        0.4000000059604645,
+                                                        1
+                                                    ],
+                                                    "fillRule": "nonzero"
+                                                },
+                                                {
+                                                    "type": "ellipse",
+                                                    "id": 990,
+                                                    "name": "Ellipse Path",
+                                                    "width": 232,
+                                                    "height": 232
+                                                }
+                                            ]
+                                        },
+                                        {
+                                            "type": "shape",
+                                            "id": 991,
+                                            "name": "Triangle",
+                                            "translation": [
+                                                160.5,
+                                                -14.5
+                                            ],
+                                            "blendMode": "srcOver",
+                                            "drawOrder": 2,
+                                            "children": [
+                                                {
+                                                    "type": "colorStroke",
+                                                    "id": 992,
+                                                    "name": "Color",
+                                                    "opacity": 1,
+                                                    "color": [
+                                                        0.800000011920929,
+                                                        0.800000011920929,
+                                                        0.800000011920929,
+                                                        1
+                                                    ],
+                                                    "width": 1,
+                                                    "cap": "butt",
+                                                    "join": "miter",
+                                                    "trim": "off",
+                                                    "trimStart": 0,
+                                                    "trimEnd": 1,
+                                                    "trimOffset": 0
+                                                },
+                                                {
+                                                    "type": "colorFill",
+                                                    "id": 993,
+                                                    "name": "Color",
+                                                    "opacity": 1,
+                                                    "color": [
+                                                        0.4000000059604645,
+                                                        0.4000000059604645,
+                                                        0.4000000059604645,
+                                                        1
+                                                    ],
+                                                    "fillRule": "nonzero"
+                                                },
+                                                {
+                                                    "type": "triangle",
+                                                    "id": 994,
+                                                    "name": "Triangle Path",
+                                                    "width": 304,
+                                                    "height": 278
+                                                }
+                                            ]
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                },
+                "assets": [],
+                "settings": {}
+            });
+        });
+    }
+}
--- tool/index.js
+++ tool/index.js
@@ -1,8 +1,173 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import MonacoEditor from 'react-monaco-editor';
+import bind from "bind";
+import download from "downloadjs";
+import JSZip from "jszip";
+import LottieToFlare from "../converter/lottie_to_flare.js";
+import "./style.css";
 
-const App = () => {
-  return <div>Hello React, Webpack 4 & Babel 7!</div>;
-};
+class App extends React.Component
+{
+	constructor(props)
+	{
+		super(props);
+		this.state = {
+			code: '{"test":true}',
+		}
+	}
+
+	componentDidMount()
+	{
+		window.addEventListener("resize", this.onResizeWindow);
+	}
+
+	componentWillUnmount()
+	{
+		window.removeEventListener("resize", this.onResizeWindow);
+	}
+
+	@bind
+	onResizeWindow(ev)
+	{
+		const { lottieEditor, flareEditor } = this;
+		if (lottieEditor) { lottieEditor.layout(); }
+		if (flareEditor) { flareEditor.layout(); }
+	}
+
+	@bind
+	lottieEditorDidMount(editor, monaco)
+	{
+		this.lottieEditor = editor;
+		editor.focus();
+		this.convert();
+	}
+
+	@bind
+	flareEditorDidMount(editor, monaco)
+	{
+		this.flareEditor = editor;
+		this.convert();
+	}
+
+	@bind
+	onChange(newValue, e)
+	{
+		clearTimeout(this.convertTimeout);
+		this.convertTimeout = setTimeout(this.convert, 1000);
+	}
+
+	@bind
+	convert()
+	{
+		const { lottieEditor, flareEditor } = this;
+		if (!lottieEditor || !flareEditor)
+		{
+			return;
+		}
+		const lottie = lottieEditor.getValue();
+
+		const converter = new LottieToFlare();
+		converter.convert(lottie).then((flareJson) =>
+		{
+			flareEditor.setValue(JSON.stringify(flareJson, null, 2));
+		});
+	}
+
+	@bind
+	getLottieFromURL()
+	{
+		const { lottieEditor } = this;
+		if (!lottieEditor)
+		{
+			return;
+		}
+
+		const url = prompt("Lottie URL:", "https://assets4.lottiefiles.com/temp/lf20_XPFCL2.json");
+		if (!url)
+		{
+			return;
+		}
+		const req = new XMLHttpRequest();
+		req.addEventListener("load", () =>
+		{
+			let text = null;
+			try
+			{
+				const json = JSON.parse(req.responseText);
+				text = JSON.stringify(json, null, 2);
+			}
+			catch (e)
+			{
+				alert("Failed to get lottie data.");
+				return;
+			}
+
+
+			this.lottieEditor.setValue(text);
+
+		});
+		req.open("GET", url);
+		req.send();
+	}
+
+	@bind
+	downloadFlare()
+	{
+		const { flareEditor } = this;
+		if(!flareEditor)
+		{
+			return;
+		}
+		const value = flareEditor.getValue();
+		const name = "name.flr2d";
+		
+		const zip = JSZip();
+		zip.file(name, value);
+		zip.generateAsync({type:"blob"}).then(function(content) {
+			download(content, name, "application/octet-stream");
+		});
+		
+	}
+
+	render()
+	{
+		const code = this.state.code;
+		const options = {
+			selectOnLineNumbers: true
+		};
+		return (
+			<div className="panels">
+				<div className="codePanel">
+					<div className="codeHeader"><span>Lottie JSON</span><div onClick={this.getLottieFromURL}>From URL</div></div>
+					<MonacoEditor
+						ref={this.setLottieCode}
+						width="100%"
+						height="100%"
+						language="json"
+						theme="vs-dark"
+						value={code}
+						options={{ selectOnLineNumbers: true }}
+						onChange={this.onChange}
+						editorDidMount={this.lottieEditorDidMount} />
+				</div>
+				<div className="codePanel">
+					<div className="codeHeader"><span>Flare JSON</span><div onClick={this.downloadFlare}>Download</div></div>
+					<MonacoEditor
+						ref={this.setFlareCode}
+						width="100%"
+						height="100%"
+						language="json"
+						theme="vs-dark"
+						options={{
+							selectOnLineNumbers: true,
+							readOnly: true
+						}}
+						editorDidMount={this.flareEditorDidMount} />
+				</div>
+			</div>
+		);
+	}
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
