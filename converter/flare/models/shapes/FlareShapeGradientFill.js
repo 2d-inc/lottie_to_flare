672c68d4d8e43eb1b5892b8fa7f7210d3b3bfd51
airnan <hernantorrisi@gmail.com>
2019-09-05 21:44:21 -0400

    skipping animating start and end points

--- converter/flare/models/shapes/FlareShapeGradientFill.js
+++ converter/flare/models/shapes/FlareShapeGradientFill.js
@@ -23,14 +23,20 @@ export default class FlareShapeGradientFill {
 		const opacity = convertProperty(paintData.opacity, propertyTypes.OPACITY, animations, id, 0.01, offsetTime);
 		node.opacity = opacity
 
-		const start = convertProperty(paintData.startPoint, propertyTypes.GRADIENT_START, animations, id, 1, offsetTime);
-		const end = convertProperty(paintData.endPoint, propertyTypes.GRADIENT_END, animations, id, 1, offsetTime);
+		// TODO: Start and End point can't be animated independently from gradient values.
+		// The only ones that could be animated are the ones that share same keyframes and interpolatiosn
+		// for now, I'm keeping static values
+		const start = paintData.startPoint.animated ? paintData.startPoint.firstValue : paintData.startPoint.value;
+		const end = paintData.endPoint.animated ? paintData.endPoint.firstValue : paintData.endPoint.value;
+		// const start = convertProperty(paintData.startPoint, propertyTypes.GRADIENT_START, animations, id, 1, offsetTime);
+		// const end = convertProperty(paintData.endPoint, propertyTypes.GRADIENT_END, animations, id, 1, offsetTime);
+		
 		const colorStops = this._GradientStops.convert(animations, offsetTime, id, start, end);
 
 
 		const radialProperties = {}
 
-		//TODO: find out if radial properties are supported
+		// TODO: find out if radial properties are supported
 		/*if (gradientType === 2) {
 			const secondaryRadiusScale = convertProperty(paintData.highlightLength, propertyTypes.GRADIENT_HIGHLIGHT_LENGTH, animations, id, 0.01, offsetTime);
 			radialProperties.secondaryRadiusScale = secondaryRadiusScale
